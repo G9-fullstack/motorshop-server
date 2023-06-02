0604942627da842d11c3f6be3a3c556b6a329ab6
Gabriel Pavão <gabpavao@outlook.com>
2023-06-02 18:12:21 -0300

    feat: call constructor to receive prisma service in announce prisma repository

--- src/modules/announces/repositories/prisma/announce-prisma.repository.ts
+++ src/modules/announces/repositories/prisma/announce-prisma.repository.ts
@@ -3,26 +3,29 @@ import { AnnounceRepository } from "../announce.repository";
 import { CreateAnnounceDto } from "../../dto/create-announce.dto";
 import { UpdateAnnounceDto } from "../../dto/update-announce.dto";
 import { Announce } from "../../entities/announce.entity";
+import { PrismaService } from "src/server/prisma.service";
 
 @Injectable()
 export class AnnouncePrismaRepository implements AnnounceRepository {
-	create(data: CreateAnnounceDto): Promise<Announce> {
+	constructor(private prisma: PrismaService) {}
+
+	async create(data: CreateAnnounceDto): Promise<Announce> {
 		throw new Error("Method not implemented.");
 	}
 
-	findAll(): Promise<Announce[]> {
+	async findAll(): Promise<Announce[]> {
 		throw new Error("Method not implemented.");
 	}
 
-	findOne(id: number): Promise<Announce> {
+	async findOne(id: number): Promise<Announce> {
 		throw new Error("Method not implemented.");
 	}
 
-	update(id: number, data: UpdateAnnounceDto): Promise<Announce> {
+	async update(id: number, data: UpdateAnnounceDto): Promise<Announce> {
 		throw new Error("Method not implemented.");
 	}
 
-	remove(id: number): Promise<void> {
+	async remove(id: number): Promise<void> {
 		throw new Error("Method not implemented.");
 	}
 }
