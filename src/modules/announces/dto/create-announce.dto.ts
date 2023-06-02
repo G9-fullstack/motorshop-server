5e53613faa03bc3b8888d706596d1b7fa30ad163
Gabriel Pavão <gabpavao@outlook.com>
2023-06-02 17:00:50 -0300

    feat: create Announce entity and dto

--- src/modules/announces/dto/create-announce.dto.ts
+++ src/modules/announces/dto/create-announce.dto.ts
@@ -1,1 +1,71 @@
-export class CreateAnnounceDto {}
+import {
+	IsString,
+	IsNotEmpty,
+	IsEnum,
+	MaxLength,
+	Length,
+	IsInt,
+	IsNumberString,
+	IsPositive,
+	IsNumber,
+	Min
+} from "class-validator";
+
+enum Brand {
+	chevrolet,
+	citroen,
+	fiat,
+	ford,
+	honda,
+	hyundai,
+	nissan,
+	peugeot,
+	renault,
+	toyota,
+	volkswagen
+}
+
+export class CreateAnnounceDto {
+	@IsString()
+	@IsNotEmpty()
+	@IsEnum(Brand)
+		brand: Brand;
+
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(127)
+		model: string;
+
+	@IsNumberString()
+	@IsNotEmpty()
+	@Length(4)
+		year: string;
+
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(10)
+		mileage: string;
+
+	@IsInt()
+	@IsPositive()
+	@IsNotEmpty()
+		fuel: number;
+
+	@IsString()
+	@IsNotEmpty()
+	@MaxLength(25)
+		color: string;
+
+	@IsNumber()
+	@IsPositive()
+	@Min(0)
+		price: number;
+
+	@IsString()
+	@IsNotEmpty()
+		description: string;
+
+	@IsString()
+	@IsNotEmpty()
+		coverImage: string;
+}
--- src/modules/announces/entities/announce.entity.ts
+++ src/modules/announces/entities/announce.entity.ts
@@ -1,1 +1,28 @@
-export class Announce {}
+enum Brand {
+	chevrolet,
+	citroen,
+	fiat,
+	ford,
+	honda,
+	hyundai,
+	nissan,
+	peugeot,
+	renault,
+	toyota,
+	volkswagen
+}
+
+export class Announce {
+	id: number;
+	brand: Brand;
+	model: string;
+	year: string;
+	mileage: string;
+	fuel: number;
+	color: string;
+	price: number;
+	description: string;
+	coverImage: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
