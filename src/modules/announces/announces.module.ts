329142c6c9775514d4d0ac92c0060e1b97d24ee1
Gabriel Pavão <gabpavao@outlook.com>
2023-06-02 17:56:08 -0300

    feat: insert announce repositories to the providers array in the announce module file

--- src/modules/announces/announces.module.ts
+++ src/modules/announces/announces.module.ts
@@ -1,9 +1,17 @@
 import { Module } from "@nestjs/common";
 import { AnnouncesService } from "./announces.service";
 import { AnnouncesController } from "./announces.controller";
+import { AnnounceRepository } from "./repositories/announce.repository";
+import { AnnouncePrismaRepository } from "./repositories/prisma/announce-prisma.repository";
 
 @Module({
 	controllers: [AnnouncesController],
-	providers: [AnnouncesService],
+	providers: [
+		AnnouncesService,
+		{
+			provide: AnnounceRepository,
+			useClass: AnnouncePrismaRepository,
+		}
+	],
 })
 export class AnnouncesModule {}
