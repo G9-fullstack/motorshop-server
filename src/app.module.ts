3470b31634679bd3d5666d008d5ef21c06653ee2
Gabriel Pavão <gabpavao@outlook.com>
2023-06-12 19:03:04 -0300

    feat: Users module set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AnnouncesModule } from "./modules/announces/announces.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @Module({
-	imports: [AnnouncesModule],
+	imports: [AnnouncesModule, UsersModule],
 })
 export class AppModule {}
