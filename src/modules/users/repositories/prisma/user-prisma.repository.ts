4e7c44d7fcd153d3c27ba9b02b18826190998472
Gabriel Pavão <gabpavao@outlook.com>
2023-06-12 19:40:14 -0300

    feat: create abstract UserRepository and implement it in a prisma repository

---
+++ src/modules/users/repositories/prisma/user-prisma.repository.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { UserRepository } from "../user.repository";
+import { CreateUserDto } from "../../dto/create-user.dto";
+import { UpdateUserDto } from "../../dto/update-user.dto";
+import { User } from "../../entities/user.entity";
+import { PrismaService } from "src/server/prisma.service";
+
+@Injectable()
+export class UserPrismaRepository implements UserRepository {
+	constructor(private prisma: PrismaService) {}
+
+	async create(data: CreateUserDto): Promise<User> {
+		throw new Error("Method not implemented.");
+	}
+	async findAll(): Promise<User[]> {
+		throw new Error("Method not implemented.");
+	}
+	async findOne(id: number): Promise<User> {
+		throw new Error("Method not implemented.");
+	}
+	async update(id: number, data: UpdateUserDto): Promise<User> {
+		throw new Error("Method not implemented.");
+	}
+	async delete(id: number): Promise<void> {
+		throw new Error("Method not implemented.");
+	}
+}
