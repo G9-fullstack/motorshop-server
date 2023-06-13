b7b373dbed9ba216ff7b462ecf3809f729db8cae
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-13 16:47:36 -0300

    create user and address implemented

--- src/modules/users/repositories/prisma/user-prisma.repository.ts
+++ src/modules/users/repositories/prisma/user-prisma.repository.ts
@@ -8,22 +8,20 @@ import { PrismaService } from "src/server/prisma.service";
 @Injectable()
 export class UserPrismaRepository implements UserRepository {
 	constructor(private prisma: PrismaService) {}
-	findAll(): User[] | Promise<User[]> {
+	findAll(): Promise<User[]> {
 		throw new Error("Method not implemented.");
 	}
-	findOne(id: number): User | Promise<User> {
+	findOne(id: number): Promise<User> {
 		throw new Error("Method not implemented.");
 	}
-	update(id: number, data: UpdateUserDto): User | Promise<User> {
+	update(id: number, data: Partial<UpdateUserDto>): Promise<User> {
 		throw new Error("Method not implemented.");
 	}
-	delete(id: number): void | Promise<void> {
+	delete(id: number): Promise<void> {
 		throw new Error("Method not implemented.");
 	}
-
 	async create(data: CreateUserDto): Promise<User> {
 		const {address, ...user} = data;
-		// console.log(address, user);
 		const userCreated = await this.prisma.user.create({
 			data: {
 				address: {
@@ -40,29 +38,3 @@ export class UserPrismaRepository implements UserRepository {
 		return userCreated;
 	}}
 
-
-// const newAnnounce = await this.prisma.announce.create({
-// 	data: {
-// 		...announce,
-// 		sellerId: 3,
-// 	},
-// });
-// return newAnnounce;
-
-
-
-
-
-// 	async findAll(): Promise<User[]> {
-// 		throw new Error("Method not implemented.");
-// 	}
-// 	async findOne(id: number): Promise<User> {
-// 		throw new Error("Method not implemented.");
-// 	}
-// 	async update(id: number, data: UpdateUserDto): Promise<User> {
-// 		throw new Error("Method not implemented.");
-// 	}
-// 	async delete(id: number): Promise<void> {
-// 		throw new Error("Method not implemented.");
-// 	}
-// }
