bd777cbc1f80e9231cca600704f2f7381a7be0f0
Gabriel Pavão <gabpavao@outlook.com>
2023-06-13 11:33:05 -0300

    feat: create auth service to validate user credentials

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from "@nestjs/common";
+import { UsersService } from "../users/users.service";
+import { compare } from "bcryptjs";
+import { User } from "../users/entities/user.entity";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private usersService: UsersService) {}
+
+	async validateUser(userEmail: string, userPassword: string): Promise<Pick<User, "email">> | null {
+		const user = await this.usersService.findByEmail(userEmail);
+
+		if (user) {
+			const isValidPassword: boolean = await compare(userPassword, user.password);
+
+			if (isValidPassword) {
+				return { email: user.email, };
+			}
+		}
+
+		return null;
+	}
+}
