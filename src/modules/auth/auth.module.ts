f4b2b59fc5d47e00fc09fa0f098a5c484317e3a3
Gabriel Pavão <gabpavao@outlook.com>
2023-06-13 12:16:05 -0300

    feat: create jwt strategy and auth guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UsersModule } from "../users/users.module";
 import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { LocalStrategy } from "./local.strategy";
+import { JwtStrategy } from "./jwt.strategy";
+import "dotenv/config";
 
 @Module({
-	imports: [UsersModule, PassportModule],
+	imports: [
+		UsersModule,
+		PassportModule,
+		JwtModule.register({
+			secret: process.env.SECRET_KEY,
+			signOptions: { expiresIn: "24h", },
+		})
+	],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
