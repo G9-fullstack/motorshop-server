37341d38d146278997a80f74588f394aed794380
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-14 14:44:29 -0300

    feat: cpf, phoneNumber and email unique validation created. password return locked

--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -3,10 +3,13 @@ import { UpdateUserDto } from "../dto/update-user.dto";
 import { User } from "../entities/user.entity";
 
 export abstract class UserRepository {
+
   abstract create(data: CreateUserDto): Promise<User>
   abstract findAll(): Promise<User[]>
   abstract findOne(id: number): Promise<User>
   abstract findByEmail(email: string): Promise<User>;
+  abstract findByCpf(cpf: string): Promise<User>;
+  abstract findByPhoneNumber(phoneNumber: string): Promise<User>;
   abstract update(id: number, data: UpdateUserDto): Promise<User>
   abstract delete(id: number): Promise<void>
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UserRepository } from "./repositories/user.repository";
@@ -8,6 +8,20 @@ export class UsersService {
 	constructor(private userRepository: UserRepository) {}
 
 	async create(createUserDto: CreateUserDto) {
+		const emailExists = await this.userRepository.findByEmail(createUserDto.email);
+		const cpfExists = await this.userRepository.findByCpf(createUserDto.cpf);
+		const phoneNumberExists = await this.userRepository.findByPhoneNumber(createUserDto.phoneNumber);
+
+		if(emailExists){
+			throw new ConflictException("Email already exists");
+		}
+		if(cpfExists){
+			throw new ConflictException("CPF number already exists");
+		}
+		if(phoneNumberExists){
+			throw new ConflictException("Phone number already  exists");
+		}
+
 		return await this.userRepository.create(createUserDto);
 	}
 
@@ -24,6 +38,7 @@ export class UsersService {
 
 		return user;
 	}
+
 
 	async update(id: number, updateUserDto: UpdateUserDto) {
 		return `This action updates a #${id} user`;
