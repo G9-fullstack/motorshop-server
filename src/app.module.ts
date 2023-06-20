fd044504cafa74a0b73b0ae29d854514308462db
leocarlos-dias <leocsdias@hotmail.com>
2023-06-20 16:22:52 -0300

    feat: add seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AnnouncesModule } from "./modules/announces/announces.module";
-import { UsersModule } from "./modules/users/users.module";
 import { AuthModule } from "./modules/auth/auth.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @Module({
 	imports: [AnnouncesModule, UsersModule, AuthModule],
 })
-export class AppModule {}
+export class AppModule { }
