dc15f1a7ebbce92502453ec3d452e3c2da84186b
vitor-galdino <httpvittor@gmail.com>
2023-06-21 18:07:01 -0300

    feat: add class findOne

--- src/modules/address/repositories/address.repository.ts
+++ src/modules/address/repositories/address.repository.ts
@@ -2,5 +2,6 @@ import { UpdateAddressDto } from "../dto/update-address.dto";
 import { Address } from "../entities/address.entity";
 
 export abstract class AddressRepository {
+  abstract findOne(id: number): Promise<Address>
   abstract update(id: number, data: UpdateAddressDto): Promise<Address>;
 }
