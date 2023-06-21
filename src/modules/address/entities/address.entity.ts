ca3f5bda39d7bcfadfbd6b1a3c2ee4e5808164d8
vitor-galdino <httpvittor@gmail.com>
2023-06-21 17:28:10 -0300

    feat: add address entity

--- src/modules/address/entities/address.entity.ts
+++ src/modules/address/entities/address.entity.ts
@@ -1,1 +1,9 @@
-export class Address {}
+export class Address {
+	readonly id: number;
+	zipCode: string;
+	state: string;
+	city: string;
+	street: string;
+	number: string;
+	complement?: string;
+}
