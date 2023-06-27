475415ab4c7ebfe0e8085064e7685c9537b37050
leocarlos-dias <leocsdias@hotmail.com>
2023-06-27 10:18:01 -0300

    doc: add initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	const config = new DocumentBuilder()
+		.setTitle("Motorshop API")
+		.setDescription("The Motorshop API is a RESTful API for managing motorshop data.")
+		.setVersion("1.0")
+		.addTag("cars", "The cars API")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+
+	SwaggerModule.setup("api", app, document);
 	app.enableCors();
 
 	app.useGlobalPipes(
