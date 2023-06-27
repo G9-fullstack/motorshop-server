e6e6393d05314fca627f9019b70f53c1dc661284
leocarlos-dias <leocsdias@hotmail.com>
2023-06-27 14:57:32 -0300

    doc: add auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,19 +4,58 @@ import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { JwtAuthGuard } from "./jwt-auth.guard";
 import { LocalAuthGuard } from "./local-auth.guard";
+import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Auth")
 @Controller()
 export class AuthController {
 	constructor(private readonly authService: AuthService) { }
 
-	@Post("/login")
+	@ApiOkResponse({
+		description: "User logged in successfully", schema: {
+			type: "object",
+			properties: {
+				token: {
+					type: "string",
+					example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+				},
+			},
+		},
+	})
+	@ApiBody({ type: LoginDto, })
+	@ApiBearerAuth()
 	@UseGuards(LocalAuthGuard)
+	@Post("/login")
 	async login(@Body() user: LoginDto) {
 		return this.authService.login(user.email);
 	}
 
-	@Get("/profile")
+	@ApiOkResponse({
+		description: "User profile retrieved successfully", schema: {
+			type: "object",
+			properties: {
+				id: {
+					type: "integer",
+					example: 1,
+				},
+				name: {
+					type: "string",
+					example: "John Doe",
+				},
+				description: {
+					type: "string",
+					example: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+				},
+				isSeller: {
+					type: "boolean",
+					example: true,
+				},
+			},
+		},
+	})
+	@ApiBearerAuth()
 	@UseGuards(JwtAuthGuard)
+	@Get("/profile")
 	async getProfile(@Req() request: Request) {
 		return await this.authService.validateToken(request.user);
 	}
--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDto {
+	@ApiProperty({ type: "string", example: "jonhdoe@example.com", description: "Email", })
 	@IsEmail()
 	@IsNotEmpty()
 		email: string;
 
+	@ApiProperty({ type: "string", example: "123456", description: "Password", })
 	@IsString()
 	@IsNotEmpty()
 		password: string;
