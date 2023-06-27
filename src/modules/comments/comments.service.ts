936eaa57f6df24f2629e9e8917cf072336efe7a6
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2023-06-27 10:06:57 -0300

    feat/route post comment created

--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,13 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from "@nestjs/common";
 import { CreateCommentDto } from "./dto/create-comment.dto";
+import { CommentRepository } from "./repositories/comment.repository";
 
 @Injectable()
 export class CommentsService {
-	create(createCommentDto: CreateCommentDto) {
-		return "This action adds a new comment";
+	constructor(private commentRepository: CommentRepository) { }
+
+	async create(createCommentDto: CreateCommentDto, userInfo: any, announceId: any ) {
+		return await this.commentRepository.create(userInfo.id, createCommentDto, announceId);
 	}
 
 	findAll() {
 		return "This action returns all comments";
 	}
 }
+
--- src/modules/comments/repositories/prisma/comment-prisma.repository.ts
+++ src/modules/comments/repositories/prisma/comment-prisma.repository.ts
@@ -2,13 +2,28 @@ import { Injectable } from "@nestjs/common";
 import { CreateCommentDto } from "../../dto/create-comment.dto";
 import { Comment } from "../../entities/comment.entity";
 import { CommentRepository } from "../comment.repository";
+import { PrismaService } from "src/server/prisma.service";
 
 
 @Injectable()
 export class CommentPrismaRepository implements CommentRepository {
-	async create(announceId: number, data: CreateCommentDto): Promise<Comment> {
-		throw new Error("Method not implemented.");
+	constructor(private prisma: PrismaService) { }
+	async create(userId, data: CreateCommentDto, announceId: number ): Promise<Comment> {
+
+		const commentData = {
+			user: { connect: { id: userId, }, },
+			announce: { connect: { id: announceId, }, },
+			comment: data.comment,
+		};
+
+		const commentCreated = await this.prisma.comment.create({
+			data: commentData,
+		});
+
+		return commentCreated;
 	}
+
+
 
 	async findAll(announceId: number): Promise<Comment[]> {
 		throw new Error("Method not implemented.");
