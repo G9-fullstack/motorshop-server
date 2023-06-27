89cdb883fc5f0310854d407910e7fc38a77e0591
leocarlos-dias <leocsdias@hotmail.com>
2023-06-27 14:52:54 -0300

    doc: add address controller

--- src/modules/address/dto/create-address.dto.ts
+++ src/modules/address/dto/create-address.dto.ts
@@ -1,27 +1,34 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateAddressDto {
 
+  @ApiProperty({ type: "string", example: "12345678", description: "Zip code", })
   @IsString()
   @IsNotEmpty()
   	zipCode: string;
 
+  @ApiProperty({ type: "string", example: "SP", description: "State", })
   @IsString()
   @IsNotEmpty()
   	state: string;
 
+  @ApiProperty({ type: "string", example: "São Paulo", description: "City", })
   @IsString()
   @IsNotEmpty()
   	city: string;
 
+  @ApiProperty({ type: "string", example: "Rua dos Bobos", description: "Street", })
   @IsString()
   @IsNotEmpty()
   	street: string;
 
+  @ApiProperty({ type: "string", example: "123", description: "Number", })
   @IsString()
   @IsNotEmpty()
   	number: string;
 
+  @ApiProperty({ type: "string", example: "Casa", description: "Complement", })
   @IsString()
   @IsOptional()
   	complement?: string;
