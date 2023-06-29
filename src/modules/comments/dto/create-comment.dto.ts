7fa2cec7d54f33520bb6adc16a869449b5174536
aletanus <alessandro@altadesign.com.br>
2023-06-29 12:06:12 -0300

    feat: route for comments added to the documentation.

--- src/modules/comments/dto/create-comment.dto.ts
+++ src/modules/comments/dto/create-comment.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateCommentDto {
+	@ApiProperty({ description: "User comment on an announcement", example: "I've owned a previous model from this brand, and it has been incredibly reliable", } )
 	@IsString()
 	@IsNotEmpty()
 		comment: string;
