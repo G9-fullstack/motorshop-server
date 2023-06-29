cc9c1b29a8b923715735b16b63d75645b19f4114
aletanus <alessandro@altadesign.com.br>
2023-06-29 14:21:57 -0300

    Just added a new class at comments.controller.ts to reflect in the documentation the exact data returned from the api in resposponse to a successful comment creation.

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Post, Body, Request, UseGuards, Param } from "@nestjs/common";
 import { CommentsService } from "./comments.service";
 import { CreateCommentDto } from "./dto/create-comment.dto";
-import { ApiBody, ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiCreatedResponse, ApiOperation, ApiProperty, ApiTags } from "@nestjs/swagger";
 import * as Express from "express";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import { Comment } from "./entities/comment.entity";
+
+class CommentResponseDto extends Comment {
+	@ApiProperty({ description: "User ID", example: 2, })
+		userId: number;
+	@ApiProperty({ description: "Announce ID", example: 1, })
+		announceId: number;
+}
 
 @ApiTags("comments")
 @UseGuards(JwtAuthGuard)
@@ -13,7 +20,7 @@ export class CommentsController {
 	constructor(private readonly commentsService: CommentsService) {}
 
 	@ApiOperation({ summary: "Create a new comment", })
-	@ApiCreatedResponse({ description: "Comment created successfully", type: Comment, })
+	@ApiCreatedResponse({ description: "Comment created successfully", type: CommentResponseDto, })
 	@ApiBody({ type: CreateCommentDto, })
 	@Post()
 	create(@Request() req: Express.Request, @Body() createCommentDto: CreateCommentDto, @Param("id") id: string) {
